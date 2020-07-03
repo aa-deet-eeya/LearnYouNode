96a46c196cd2c38745969609b1b623840152fda2
aa-deet-eeya <aditya99mondal@gmail.com>
2020-07-03 12:59:59 +0530

    added solutions

---
+++ 7-http-client.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const http = require('http') ;
+
+
+http.get( process.argv[2] , (res)=>{
+
+	res.setEncoding('utf-8') ;
+
+	res.on('data' , chunk=>{
+	
+		console.log(chunk) ;
+	} ) ;
+
+
+	res.on('end', ()=>{
+		try {}
+		catch(e) {
+			console.log(e) ;
+		}
+
+	})
+
+}) ;
